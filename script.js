e1730e8985b5be6f021e5c0f97fa882243c41fad
luis Colina <79709581+Silkiercomet@users.noreply.github.com>
2023-05-15 10:34:15 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const perks=[
+    {   
+        number: `1`,
+        title:`Actionable insights`,
+        description: `Optimize your products, improve customer satisfaction and stay ahead of the competition 
+        with our product data analytics.`
+    },
+    {
+        number:`2`,
+        title: `Data-driven decisions`,
+        description: `Make data-driven decisions with our product data analytics. Our AI-generated reports help 
+        you unlock insights hidden in your product data.`
+    },
+    {
+        number:`3`,
+        title: `Always affordable`,
+        description: `Always affordable pricing that scales with your business. Get top-quality product data 
+        analytics services without hidden costs or unexpected fees.`
+    }
+]
